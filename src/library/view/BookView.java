0d2aaaa9aedf848e23d8cab3a5a0e457f8314ac2
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-15 17:16:28 +0900

    최초 커밋

---
+++ src/library/view/BookView.java
@@ -0,0 +1,15 @@
+package library.view;
+
+import library.controller.BookController;
+
+public class BookView {
+    // 1) 싱글톤
+    private BookView(){}
+    private static final BookView instance = new BookView();
+    public static BookView getInstance(){
+        return instance;
+    }
+
+    // 2) controller 싱글톤 가져오기
+    private BookController bookController = BookController.getInstance();
+}
